c60a399e89ba18b11351711c0724a17395c09853
levanhoang07 <levanhoang742004@gmail.com>
2024-05-09 10:45:25 +0700

    ls6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import LvhProductlist from './component/LvhProductlist';
+import LvhProductAdd from './component/LvhProductAdd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      products : [
+        { title: 'Lê Văn Hoàng', id: 2210900024 , status:1 },
+        { title: 'Cabbage', id: 1 , status:1 },
+        { title: 'Garlic', id: 2 , status:0 },
+        { title: 'Apple', id: 3 , status:0 },
+        { title: 'Samsung', id: 4 , status:1 },
+      ]
+    }
+  }
+  lvhHanldSubmit=(param)=>{
+    console.log("App:",param);
+    //thêm vào bảng dữ liệu product
+    let {products}=this.state;
+    products.push(param);
+    this.setState({
+      products:products
+    })
+  }
+  render() {
+    return (
+      <div className='container'>
+          <h1>Lê Văn Hoàng -Event Form - Render Data</h1>
+          <hr/>
+          {/* Corrected prop name from renderProduct to renderProducts */}
+          <LvhProductlist renderProducts={this.state.products}/>
+          <hr/>
+          <LvhProductAdd onSubmit={'this.lvhHanldSubmit'}/>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default  App;
